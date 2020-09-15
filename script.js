8ed8e7a4af2b57daf51340aa89de7d2acb4d2ad6
Louis Longo <69089968+LouisLongo79@users.noreply.github.com>
2020-09-15 16:06:36 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+function generate(){
+  let complexity = document.getElementById("slider").value;
+
+  let  values = "ABCDEFGHIJKPMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*";
+  
+  let password= "";
+  
+  for(var i = 0; i <= complexity; i++) {
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+    }
+    
+    document.getElementById("display").value = password;
+
+
+}
